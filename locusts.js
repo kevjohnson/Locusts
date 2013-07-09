b4d54b426e7ae315c394b4e1bd59908400eb32a2
Joseph Elsherbini <joseph.elsherbini@gmail.com>
2013-07-08 22:48:40 -0400

    useless prettification

--- locusts.js
+++ locusts.js
@@ -1,3 +1,7 @@
  $(document).ready(function() {
-  $("#countries").select2();
+  $("#countries").select2(
+      {placeholder: "Select a Country"});
+    $("#disaster").select2(
+        {placeholder: "Select a Disaster",
+        allowClear: true});
  });
